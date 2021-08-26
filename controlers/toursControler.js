49d76d80dc90f510a6bd124433a4c10edcf7e27d
Salim255 <79255969+Salim255@users.noreply.github.com>
2021-08-26 17:46:36 +0200

    ADDING 404  NOT FOUND ERROR

--- controlers/toursControler.js
+++ controlers/toursControler.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -29,6 +30,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
     .limitFields()
     .paginate();
   const tours = await features.query;
+  
   //SEND RESPONSE
   res.status(200).json({
     status: 'success',
@@ -44,6 +46,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const id = req.params.id;
   const tour = await Tour.findById(id);
+  if(!tour){
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -74,6 +79,9 @@ exports.updatTour = catchAsync(async (req, res, next) => {
     runValidator: true,
   });
 
+  if (!tour) {
+      return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -85,8 +93,10 @@ exports.updatTour = catchAsync(async (req, res, next) => {
 //To delete un object form an API
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
-
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+   if (!tour) {
+     return next(new AppError('No tour found with that ID', 404));
+   }
   res.status(204).json({
     //for delte we use 204, means no contaits
     status: 'success',
