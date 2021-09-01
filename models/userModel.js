53f14aee1a90a772db81f6351ed05107ff488a14
Salim255 <79255969+Salim255@users.noreply.github.com>
2021-09-01 15:05:58 +0200

    MANGING PASSWORD

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');//Its a validator coming from npm
+const bcrypt = require('bcryptjs');
 //1) create userSchema
 const userSchema = new mongoose.Schema(
   {
@@ -24,11 +25,35 @@ const userSchema = new mongoose.Schema(
     },
     passwordConfirm: {
       type: String,
-      required:[true, 'Please confirm your password']
+      required:[true, 'Please confirm your password'],
+      validate:{
+        //This only work on CREATE and SAVE
+        validator: function(el){
+          return el === this.password;//remember This only work in save()!!
+        },
+        message: 'The password are not the same!'
+      }
     },
-  },
-  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+  }
 );
+
+/************************ ENCRYPTING THE PASSWORD***********************/
+//W'll use mongoose middleware, and the one that we gonna use is a pre-save middleware
+userSchema.pre('save',async function(next){//we need NEXT in order to call the next middleware
+
+  //Only run this function if password was actually modified
+    if(!this.isModified('password')) return next();
+
+    /****we need to install bcryptjs package in order to use encrypting: npm i bcryptjs *******/
+
+    //Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);//using hash to ecncrypt the password
+    
+    //Delete the passwordConfirm field
+    this.passwordConfirm = undefined;
+
+    next();
+});//pre mean between geting the data and saving the data to the database
 
 //Creating the model out of our schema
 const User = mongoose.model('User', userSchema);
