97fc47da6f22b3bb8bbbfdd645ba57b59ed12efc
Salim255 <79255969+Salim255@users.noreply.github.com>
2021-09-01 16:33:19 +0200

    SIGNING UP USER

---
+++ notes.js
@@ -0,0 +1,7 @@
+//*USER Authenticatin and Authorization*//
+//There are many authentication methodes but  the that we are gonna use is a very modern and simple...
+//and secure approche called=> (Json Web Tokens or JWT for short).
+
+//*Json Web Tokens or JWT*//
+//Are stateless solution for authentication, so therer is no need to store any session state on the server.
+//npm install jsonwebtoken
