473d8f2adf9b7d618a1136f82b1ba5289d8b9719
Salim255 <79255969+Salim255@users.noreply.github.com>
2021-10-12 17:06:17 +0200

    BUILDING THE TOUR PAGE PART 1

--- controlers/viewsControler.js
+++ controlers/viewsControler.js
@@ -3,17 +3,26 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
   //1) Get the tour data from collection
-  const  tours = await Tour.find();
+  const tours = await Tour.find();
   //2) Build template
   //3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours
+    tours,
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  //1) get the data, for the requested tour(including reviews andguides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user', //only field that we need
+  });
+  //2) Building the template
+  console.log(tour);
+  //3)Render template using data from 1
   res.status(200).render('tour', {
     title: 'The Forset Hiker Tour',
+    tour,
   });
-};
+});
