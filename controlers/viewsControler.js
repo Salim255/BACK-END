63ef58befa8f8ea6c1489c40048c3ebed7d6a2e9
Salim255 <79255969+Salim255@users.noreply.github.com>
2021-10-13 17:27:04 +0200

    Building the login screen

--- controlers/viewsControler.js
+++ controlers/viewsControler.js
@@ -12,20 +12,29 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   //1) get the data, for the requested tour(including reviews andguides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user', //only field that we need
   });
   //2) Building the template
-  
+
   //3)Render template using data from 1
-  res.status(200).set(
+  res
+    .status(200)
+    .set(
       'Content-Security-Policy',
       "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    ).render('tour', {
-    title: `${tour.name} Tour`,
-    tour,
-  });
+    )
+    .render('tour', {
+      title: `${tour.name} Tour`,
+      tour,
+    });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
--- routes/viewsRoute.js
+++ routes/viewsRoute.js
@@ -5,4 +5,6 @@ const router = express.Router();
 router.get('/', viewsControler.getOverview);
 router.get('/tour/:slug', viewsControler.getTour);
 
+router.get('/login', viewsControler.getLoginForm);
+
 module.exports = router;
