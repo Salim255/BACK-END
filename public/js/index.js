79bddcaf547bb4bee93a92abbbd8202f88a7c380
Salim255 <79255969+Salim255@users.noreply.github.com>
2021-10-24 15:02:57 +0200

    Updating User password using our API

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,13 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 //DOM Elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 //DELEGATION
 if (mapBox) {
@@ -33,6 +34,34 @@ if (userDataForm) {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
+    updateSettings({ name, email }, '');
+  });
+}
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+    ///In order we nead that await then ...to clear the the password field in our front end
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,23 +2,25 @@
 import axios from 'axios';
 import { showAlert } from './alert';
 
-export const updateData = async (name, email) => {
+//Type is either 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+   const url =
+     type === 'password'
+       ? 'http://127.0.0.1:8000/api/v1/users/updateMyPassword'
+       : 'http://127.0.0.1:8000/api/v1/users/updateMe';
     const res = await axios({
       //axios return a promis, and when ever there are an error axios will throght the error
       method: 'PATCH',
-      url: 'http://127.0.0.1:8000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data
     });
 
     if (res.data.status === 'Success') {
-      showAlert('success', 'Data updated successfully!');
-      window.setTimeout(() => {
-        location.assign('/'); //We use this in order to load another page
-      }, 1500);
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
+      // window.setTimeout(() => {
+      //   location.assign('/me'); //We use this in order to load another page
+      // }, 1500);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
