e7b490fadd9590a66b42640128334f585afba0f6
Salim <doshka23@gmail.com>
2021-11-21 01:31:12 +0100

    COFIGRING MULTER

--- controlers/usersControler.js
+++ controlers/usersControler.js
@@ -2,6 +2,38 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+const multer = require('multer');
+
+const multerSrorage = multer.diskStorage(
+  { 
+    destination:  (req, file, cb) =>
+        {
+        cb(null, 'public/img/users' );
+        },
+    filename: (req, file, cb) =>{
+          const ext = file.mimetype.split('/')[1];
+    
+          cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
+        }
+  }
+  );//cb is just a call back function
+
+  const multerFilter = (req, file, cb) =>{
+    if(file.mimetype.startsWith('image')){
+      cb(null, true);
+    }else{
+      cb(new AppError('Not an image! Please upload only images.', 400), false)
+    }
+  }
+
+  const upload = multer({
+    storage: multerSrorage,
+    fileFilter: multerFilter
+  });
+
+
+//const upload = multer({dest: 'public/img/users'});
+exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
